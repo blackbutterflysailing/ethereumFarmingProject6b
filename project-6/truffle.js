3779b5461fc79c89d8885e59294533298f435cf1
Marie E Patterson <mariep@nebby24.com>
2019-11-23 20:42:39 -0500

    master Lates file changes

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,21 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = "alien amateur soap alarm search mercy version bitter harsh change flight erosion";
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    }, 
+    rinkeby: {
+      provider: () => 
+        new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/9999999999Mykey69e9999999999c792'),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets ) 
+      },
   }
 };
